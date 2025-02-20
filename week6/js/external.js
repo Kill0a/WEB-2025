d6c0311e412306a430d6ed3210d3c6a47cc25895
Jaylen <jaylscot@iu.edu>
2025-02-20 11:11:28 -0500

    lab1

---
+++ week6/js/external.js
@@ -0,0 +1,24 @@
+console.log("Umm... where are we?");
+
+
+const birthYear = "1207";
+
+let myName = "The Tired One";
+
+
+console.log("was born , 14");
+
+myName = "Im so tired I am fighting sleep";
+
+
+console.warn(May have devouref 10 souls );
+
+console.log("myName is" , myName.length, "charicters long.");
+
+console.log("myName includdes jay", myName.toUpperCase()includes(jay));
+
+myName = "jay";
+
+console.log("myName has uppercase", myName !== myName.toLowerCase());
+console.log("myName has lowercase", myName !== myName.toUpperCase());
+
