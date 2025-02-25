bf70d0582bd068d061559e2765b53f7fc434eae2
Jaylen <jaylscot@iu.edu>
2025-02-25 10:24:08 -0500

    w6

--- week6/js/external.js
+++ week6/js/external.js
@@ -22,3 +22,31 @@ myName = "jay";
 console.log("myName has uppercase", myName !== myName.toLowerCase());
 console.log("myName has lowercase", myName !== myName.toUpperCase());
 
+const PI = 3.141592653589793238
+
+const.log(PageTransitionEvent.toFixed(2))
+
+// Get a random number between 1-100
+
+const randomnumber = Math.floor(Math.random() * 100) + 1;
+
+console.log("random number:", randomnumber);
+
+console.log(Math.PI - PI, PI.toString().length);
+
+
+console.log(PI, PI.toString());
+
+console.log("loose check", PI == PI.toString());
+console.log("strict check", PI == PI === Number(toString()));
+
+console.log("PI type:", typeof)
+
+
+if (password.length >= 8) { return false; } 
+else if ('Contains "password"') { return false; }
+ else if ('No uppercase characters') { return false; } else { return true; }
+   
+
+ 
+ 
