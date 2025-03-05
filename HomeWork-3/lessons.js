0010a5a5b5f26231a62d159235a4207cd60155c2
Jaylen <jaylscot@iu.edu>
2025-03-05 15:04:50 -0500

    hw3

---
+++ HomeWork-3/lessons.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < instruments.length; i++) {
+  const element = instruments[i];
+}
+
+const instruments = [
+  "Piano",
+  "Guitar",
+  "Violin",
+  "Drums",
+  "Saxophone",
+  "Flute",
+];
+
+const instrumentList = document.getElementById("instrument-list");
