c8b671e3c9e2df465305e188c6a3c19474913fc9
Kill0a <jaylenscott512@icloud.com>
2025-03-05 15:12:02 -0500

    HomeWork3

---
+++ HomeWork-3/gallery.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < photoList.length; i++) {
+  const element = gallerySection[i];
+}
+
+const photoList = [
+  "https://fastly.picsum.photos/id/804/300/400.jpg?hmac=Y6cx9oentnw75-Q7gYdlaJBBAnSAlUHRYXbvFMIrZNI",
+  "https://fastly.picsum.photos/id/185/300/400.jpg?hmac=7jvc9SGhArOCJhgLoOfYSTezJsc1xrIXhDOXkiZSrbs",
+  "https://fastly.picsum.photos/id/229/300/400.jpg?hmac=CZOWn8sQ8oa9EfbMZw5tYqPMNKK0mNOYUsEgz81K8io",
+  "https://fastly.picsum.photos/id/211/300/400.jpg?hmac=l0zi1AkBxaPoAMnWav0iWH8TsWfblHXdm_IGSNDMFWQ",
+];
+
+image.src = photoList[i];
+image.alt = "image {i + i}";
+img.hight = 300;
+
+const gallerySection = document.getElementById("gallery-section");
---
+++ HomeWork-3/scores.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < score.length; i++) {
+  const element = array[i];
+}
+
+const scores = [95, 73, 88, 45, 60, 79, 82, 91, 50, 83];
+
+const averageScore = totalScore / scores.length;
+
+const scoreList = document.getElementById("score-list");
+const averageScorePairgraph = document.getElementById("average-score");
